c54dea1f8f22c0ba75431a7c8e805f60bdc4f392
Eric Kfir <eric@kfirs.com>
2019-04-24 00:00:36 +0300

    Remove "--project-directory" flag to docker composeˆ

--- bin/svc.sh
+++ bin/svc.sh
@@ -6,4 +6,4 @@ SCRIPT=${0}
 BIN_DIR=$(dirname ${SCRIPT})
 MODULE_DIR=$(cd ${BIN_DIR}/..; pwd)
 PROJECT_DIR=$(cd ${MODULE_DIR}/..; pwd)
-docker-compose --project-directory ${PROJECT_DIR} -p bluebudgetz -f ${MODULE_DIR}/svc.yaml $@
+docker-compose -p bluebudgetz -f ${MODULE_DIR}/svc.yaml $@
