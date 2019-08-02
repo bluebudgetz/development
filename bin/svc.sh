a942c283840cd6d6dd44d676f43634e65350c74d
Eric Kfir <eric@kfirs.com>
2019-08-03 00:02:37 +0300

    Refactor

--- bin/svc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-SCRIPT=${0}
-BIN_DIR=$(dirname ${SCRIPT})
-MODULE_DIR=$(cd ${BIN_DIR}/..; pwd)
-PROJECT_DIR=$(cd ${MODULE_DIR}/..; pwd)
-docker-compose -p bluebudgetz -f ${MODULE_DIR}/svc.yaml $@
